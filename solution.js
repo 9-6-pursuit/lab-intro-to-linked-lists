da38b595ebebf51d78d5d6cfe0abbe68aa289ffd
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-09-26 21:44:45 -0400

    al the class method is added

--- solution.js
+++ solution.js
@@ -14,7 +14,7 @@ class LinkedList {
 
   insert (data) {
     let newNode = new Node(data)
-    if(!this.head){
+    if(this.head === null){
       this.head = newNode
     } else {
       newNode.next = this.head
@@ -52,22 +52,41 @@ class LinkedList {
     return count;
   }
 
-  getKth(k){
-    let currentNode = this.head
-    for (let i=0; i<k; i++){
-      if(currentNode.next){
-        currentNode = currentNode.next
-      }else {
-        return undefined
-      }
+  // getKth(k){ not working
+  //   let currentNode = this.head
+  //   for (let i=1; i!==k; i++){
+  //     if(currentNode !== null){
+  //       currentNode = currentNode.next
+  //     }else {
+  //       return undefined
+  //     }
+  //   }
+  //   return currentNode.data
+  // }
+
+  getKth(k) {
+    let foundNode = this.head;
+    let counter = 1;
+    while(counter !== k){
+      counter ++
+      foundNode = foundNode.next
     }
-    return currentNode.data
+    return foundNode;
   }
 
+  // getKthToLast(k) {
+  //   let length = this.size()
+  //   return this.getKth(length - k -1)
+  // }
+
   getKthToLast(k) {
-    let length = this.size()
-    return this.getKth(length - k -1)
-  }
+    let length = this.size();
+    let foundNode = this.head;
+    for(let i=1; i<length - k; i++){
+      foundNode = foundNode.next
+    }
+    return foundNode;
+  } 
 
   clear() {
     return this.head = null;
@@ -84,38 +103,58 @@ class LinkedList {
     for (let i = 1; i < counter.length; i++) {
       prevNode = prevNode.next
     }
-    return prevNode = foundNode.next
+    return prevNode.next = foundNode.next;
   }
+
+  // delete (data) {
+  //   let node = this.head;
+  //   let counter = 0;
+  //   while (node.data !== data && node.next){
+  //     counter++
+  //     node = node.next
+  //   }
+  //   let foundNode = node 
+  //   node = this.head
+  //   for(let i=1; i<counter; i++){
+  //     node=node.next
+  //   }
+  //   return node.next = foundNode.next
+  // }
 
   isEmpty() {
-   return this.head ? false : true
+   return !this.head ? true : false
   }
 
-  toArray(linkedList) {
+  toArray() {
     let currentNode = this.head
     let arr = [];
-    while (currentNode) {
+    while (currentNode !== null) { //(currentNode)
       arr.push(currentNode.data)
       currentNode = currentNode.next
     }
     return arr;
   }
 
-  containsDuplicates(linkedList) {
-    let newLinkedList = new LinkedList();
-    let hashMap = {};
-    let currentNode = this.head
+  // containsDuplicates(linkedList) {
+  //   let newLinkedList = new LinkedList();
+  //   let hashMap = {};
+  //   let currentNode = this.head
 
-    while(currentNode){
-      if (hashMap[currentNode.data]){
-        newLinkedList.insert(currentNode.data)
-        hash[currentNode] = true
-      }
-      currentNode = currentNode.next
-    }
-    return !newLinkedList.head ? true : false;
+  //   while(currentNode){
+  //     if (hashMap[currentNode.data]){
+  //       newLinkedList.insert(currentNode.data)
+  //       hash[currentNode] = true
+  //     }
+  //     currentNode = currentNode.next
+  //   }
+  //   return !newLinkedList.head ? true : false;
+  // }
+
+  containsDuplicates() {
+    const arr = this.toArray();
+    const unique = [...new Set(arr)];
+    return arr.length !== unique.length;
   }
-
 
 }
 
