e0f6430729ca78425dc5dc2140325d6522df9208
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-09-28 14:53:54 -0400

    lab-intro-to-linked-lists

--- solution.js
+++ solution.js
@@ -70,6 +70,88 @@ class LinkedList {
       }
       return current.data;
     }
+
+    searchByKey(key) {
+      let current = this.head;
+      while (current) {
+        if (current.data === key) {
+          return current.data;
+        }
+        current = current.next;
+      }
+      return null;
+    }
+  
+    getKth(k) {
+      if (k < 0 || !this.head) {
+        return null;
+      }
+  
+      let current = this.head;
+      let count = 0;
+      while (current) {
+        if (count === k) {
+          return current.data;
+        }
+        count++;
+        current = current.next;
+      }
+      return null;
+    }
+  
+    getKthToLast(k) {
+      if (k < 0 || !this.head) {
+        return null;
+      }
+  
+      let slow = this.head;
+      let fast = this.head;
+  
+      for (let i = 0; i < k; i++) {
+        if (!fast.next) {
+          return null;
+        }
+        fast = fast.next;
+      }
+  
+      while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+  
+      return slow.data;
+    }
+  
+    isEmpty() {
+      return !this.head;
+    }
+  
+    clear() {
+      this.head = null;
+    }
+  
+    toArray() {
+      const result = [];
+      let current = this.head;
+      while (current) {
+        result.push(current.data);
+        current = current.next;
+      }
+      return result;
+    }
+  
+    containsDuplicates() {
+      const seen = new Set();
+      let current = this.head;
+      while (current) {
+        if (seen.has(current.data)) {
+          return true;
+        }
+        seen.add(current.data);
+        current = current.next;
+      }
+      return false;
+    }
 }
 
 module.exports = {
