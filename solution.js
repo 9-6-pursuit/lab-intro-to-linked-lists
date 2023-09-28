8c0e92777bed9f555eee3b901b1d282eaa8b94d5
Monir Hossen <monirhossen@pursuit.org>
2023-09-28 00:18:54 -0400

    class Node, class LinkedList, insert(), getFirst() and clear() created

--- solution.js
+++ solution.js
@@ -1,5 +1,43 @@
 const { nums, words } = require("./data/data.js");
 
+
+class Node {
+  constructor(data){
+    this.data = data
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(head =null){
+    this.head = head
+  }
+
+  clear (){
+    this.head = null
+  }
+
+  getFirst(){
+    return this.head
+  }
+
+  insert(data){
+    let newNode = new Node(data)
+    newNode.next = this.head
+    this.head = newNode
+  }
+
+
+
+
+  
+}
+
+
+
+
+
+
 module.exports = {
   Node,
   LinkedList,
