77bfa692a6b524acd40f3cad648bbf44d9e51f09
Lakisha J <lakishajohnson@pursuit.org>
2023-10-02 10:54:33 -0400

    Node w/properties created

--- solution.js
+++ solution.js
@@ -1,5 +1,13 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+
 module.exports = {
   Node,
   LinkedList,
